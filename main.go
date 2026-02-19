20e79742a81874fdc9afe5b42ad5f6ff71186cde
drogremtal <bp240@outlook.com>
2026-02-19 09:49:30 +0300

    Update main.go

--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import "fmt"
 func main() {
 	const USDTOEURO float64 = 0.88
 	const USDTORUBL float64 = 72.77
+	const EURTORUBL = USDTORUBL / USDTOEURO
 
 	///кол-во евро
 	var number float64 = 45.66
 
 	fmt.Println("Конвертация USD:", number)
-	fmt.Println(number * (1 / USDTOEURO) * USDTORUBL)
+	fmt.Println(number * EURTORUBL)
 
 }
